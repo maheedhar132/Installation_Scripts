3036698a695eba241c1c1cc26fdac6556acc4211
maheedhar talluri <maheedhar.talluri@gmail.com>
2022-09-20 01:51:50 +0530

    hashicorp installation basic script added

---
+++ hashicorp_Vault_install.sh
@@ -0,0 +1,7 @@
+sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update
+sudo apt install vault -y
+sudo systemctl start vault
+sudo systemctl status vault
+sudo systemctl enable vault
